6888d03ba63c25125725e797a4a4ebd12a61dc2b
木葉 Scarlet <kevyn031119@outlook.com>
2024-07-24 23:10:13 +0800

    D2: + 定义Service

--- azure-take-out-kt/src/main/kotlin/moe/scarlet/azure_take_out_kt/service/Interfaces.kt
+++ azure-take-out-kt/src/main/kotlin/moe/scarlet/azure_take_out_kt/service/Interfaces.kt
@@ -2,21 +2,21 @@ package moe.scarlet.azure_take_out_kt.service
 
 import com.baomidou.mybatisplus.extension.service.IService
 import moe.scarlet.azure_take_out_kt.pojo.*
-import moe.scarlet.azure_take_out_kt.pojo.dto.EmployeeDTO
-import moe.scarlet.azure_take_out_kt.pojo.dto.EmployeeEditPasswordDTO
-import moe.scarlet.azure_take_out_kt.pojo.dto.EmployeeLoginDTO
-import moe.scarlet.azure_take_out_kt.pojo.dto.EmployeePageQueryDTO
+import moe.scarlet.azure_take_out_kt.pojo.dto.*
 
 interface EmployeeService : IService<Employee> {
     fun getByUsername(username: String): Employee?
     fun login(employeeLoginDTO: EmployeeLoginDTO): Employee
-    fun save(employeeDTO: EmployeeDTO)
     fun pageQuery(employeePageQueryDTO: EmployeePageQueryDTO): QueryResult<Employee>
     fun status(status: Int, id: Long)
+    fun save(employeeDTO: EmployeeDTO)
     fun update(employeeDTO: EmployeeDTO)
     fun editPassword(employeeEditPasswordDTO: EmployeeEditPasswordDTO)
 }
 
 interface CategoryService : IService<Category> {
-
+    fun pageQuery(categoryPageQueryDTO: CategoryPageQueryDTO): QueryResult<Category>
+    fun status(status: Int, id: Long)
+    fun save(categoryDTO: CategoryDTO)
+    fun update(categoryDTO: CategoryDTO)
 }
--- azure-take-out-kt/src/main/kotlin/moe/scarlet/azure_take_out_kt/service/impl/CategoryServiceImpl.kt
+++ azure-take-out-kt/src/main/kotlin/moe/scarlet/azure_take_out_kt/service/impl/CategoryServiceImpl.kt
@@ -3,10 +3,29 @@ package moe.scarlet.azure_take_out_kt.service.impl
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl
 import moe.scarlet.azure_take_out_kt.mapper.CategoryMapper
 import moe.scarlet.azure_take_out_kt.pojo.Category
+import moe.scarlet.azure_take_out_kt.pojo.QueryResult
+import moe.scarlet.azure_take_out_kt.pojo.dto.CategoryDTO
+import moe.scarlet.azure_take_out_kt.pojo.dto.CategoryPageQueryDTO
 import moe.scarlet.azure_take_out_kt.service.CategoryService
 import org.springframework.stereotype.Service
 
 @Service
 class CategoryServiceImpl : ServiceImpl<CategoryMapper, Category>(), CategoryService {
-    
+
+    override fun pageQuery(categoryPageQueryDTO: CategoryPageQueryDTO): QueryResult<Category> {
+        TODO("Not yet implemented")
+    }
+
+    override fun status(status: Int, id: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun save(categoryDTO: CategoryDTO) {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(categoryDTO: CategoryDTO) {
+        TODO("Not yet implemented")
+    }
+
 }

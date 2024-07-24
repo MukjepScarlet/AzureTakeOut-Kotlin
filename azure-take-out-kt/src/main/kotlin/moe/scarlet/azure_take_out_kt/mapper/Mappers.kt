aaa0030f2ee6ee1a5690333c644e40260f184e8e
木葉 Scarlet <kevyn031119@outlook.com>
2024-07-25 00:58:51 +0800

    D2: + 所有Mapper

--- azure-take-out-kt/src/main/kotlin/moe/scarlet/azure_take_out_kt/mapper/Mappers.kt
+++ azure-take-out-kt/src/main/kotlin/moe/scarlet/azure_take_out_kt/mapper/Mappers.kt
@@ -1,12 +1,38 @@
 package moe.scarlet.azure_take_out_kt.mapper
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper
-import moe.scarlet.azure_take_out_kt.pojo.Category
-import moe.scarlet.azure_take_out_kt.pojo.Employee
+import moe.scarlet.azure_take_out_kt.pojo.*
 import org.apache.ibatis.annotations.Mapper
+
+@Mapper
+interface AddressBookMapper : BaseMapper<AddressBook>
+
+@Mapper
+interface CategoryMapper : BaseMapper<Category>
+
+@Mapper
+interface DishMapper : BaseMapper<Dish>
+
+@Mapper
+interface DishFlavorMapper : BaseMapper<DishFlavor>
 
 @Mapper
 interface EmployeeMapper : BaseMapper<Employee>
 
 @Mapper
-interface CategoryMapper : BaseMapper<Category>
+interface OrderDetailMapper : BaseMapper<OrderDetail>
+
+@Mapper
+interface OrdersMapper : BaseMapper<Orders>
+
+@Mapper
+interface SetMealMapper : BaseMapper<SetMeal>
+
+@Mapper
+interface SetMealDishMapper : BaseMapper<SetMealDish>
+
+@Mapper
+interface ShoppingCartMapper : BaseMapper<ShoppingCart>
+
+@Mapper
+interface UserMapper : BaseMapper<User>

2c5e9f2827a1f9c6ec6eeea4c068384bef93a6f1
木葉 Scarlet <kevyn031119@outlook.com>
2024-07-26 01:16:23 +0800

    D3: # 添加注释

--- azure-take-out-kt/src/main/kotlin/moe/scarlet/azure_take_out_kt/controller/admin/CommonController.kt
+++ azure-take-out-kt/src/main/kotlin/moe/scarlet/azure_take_out_kt/controller/admin/CommonController.kt
@@ -1,5 +1,6 @@
 package moe.scarlet.azure_take_out_kt.controller.admin
 
+import io.swagger.v3.oas.annotations.Operation
 import moe.scarlet.azure_take_out_kt.pojo.JsonResult
 import moe.scarlet.azure_take_out_kt.util.QiniuUtil
 import org.springframework.web.bind.annotation.PostMapping
@@ -14,9 +15,8 @@ class CommonController(
     private val qiniuUtil: QiniuUtil
 ) {
 
+    @Operation(summary = "上传图片")
     @PostMapping("/upload")
-    fun upload(file: MultipartFile): JsonResult<String> {
-        return JsonResult.success(qiniuUtil.getFullPath(qiniuUtil.uploadImage(file)))
-    }
+    fun upload(file: MultipartFile) = JsonResult.success(qiniuUtil.getFullPath(qiniuUtil.uploadImage(file)))
 
 }
